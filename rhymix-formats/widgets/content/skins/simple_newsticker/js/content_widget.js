a325e2c4231b1a4960244488d1356f0c77966f4c
MinSoo Kim <misol.kr@gmail.com>
2017-05-02 01:09:17 +0900

    Day 3 - news ticker 추가.
    컨텐츠 위젯을 이용한 뉴스 티커 스킨.

--- rhymix-formats/widgets/content/skins/simple_newsticker/js/content_widget.js
+++ rhymix-formats/widgets/content/skins/simple_newsticker/js/content_widget.js
@@ -1,9 +1,11 @@
-function rx_sr_ticker() {
-	$('.rx_sr_list').each(function(){
-		$('li:first-child').slideUp( function () {
-			$(this).appendTo($(this).parent('.ticker01')).slideDown();
-		});
+function rx_sr_nt_ticker() {
+	$('ul.rx_sr_nt_list').each(function(){
+		if(!$(this).attr('data-height')) {
+			$(this).css('height', $(this).height()).attr('data-height', $(this).height());
+		}
+		$(this).find('li:first-child').slideUp(400, function () {
+			$(this).appendTo($(this).parent('.rx_sr_nt_list'));
+		}).next().show(0);
 	});
-	
 }
-setInterval( function(){ rx_sr_ticker() }, 4500);
+setInterval( function(){ rx_sr_nt_ticker() }, 4500);
