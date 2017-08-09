ce5e6e0a097ac4e077924c301dfc279da1619279
MinSoo Kim <misol.kr@gmail.com>
2017-08-10 02:01:54 +0900

    회원, 커뮤니케이션, 로그인 기록 모듈 스킨

---
+++ rhymix-formats/modules/loginlog/skins/rx_prn/js/js.js
@@ -0,0 +1,19 @@
+/**
+ * @brief Check touchable device or not.
+ * @author misol <misol.kr@gmail.com>
+ **/
+function isRxPrnTouchable() {
+	var el = document.createElement('div');
+	el.setAttribute('ontouchstart', 'return;'); // or try "ontouchstart"
+	return typeof el.ontouchstart === "function";
+}
+
+$(document).ready(function() {
+	if(isRxPrnTouchable()) {
+		$(".rx_prn_loginlog div.rx_prn_tab ul.rx_prn_tab")
+			.css('white-space', 'nowrap')
+			.animate({
+				scrollLeft: $(".rx_prn_loginlog div.rx_prn_tab ul.rx_prn_tab li.active").offset().left
+			}, 300);
+	}
+});
--- rhymix-formats/modules/member/skins/rx_prn/js/member.js
+++ rhymix-formats/modules/member/skins/rx_prn/js/member.js
@@ -174,6 +174,10 @@ function isRxPrnTouchable() {
 
 $(document).ready(function() {
 	if(isRxPrnTouchable()) {
-		$(".rx_prn_member div.rx_prn_tab ul.rx_prn_tab").css('white-space', 'nowrap')
+		$(".rx_prn_member div.rx_prn_tab ul.rx_prn_tab")
+			.css('white-space', 'nowrap')
+			.animate({
+				scrollLeft: $(".rx_prn_member div.rx_prn_tab ul.rx_prn_tab li.active").offset().left
+			}, 300);
 	}
 });
